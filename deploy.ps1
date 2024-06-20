6b850f074e594706343cabdd9f712ba407298fef
mvpsupport <140552760+mvpsupport@users.noreply.github.com>
2024-06-20 12:41:45 -0500

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,3 @@
-New-AzTenantDeployment -Name "assign-support-request-contributor-role" -Location "centralus" -TemplateFile "azuredeploy.json" -TemplateParameterObject @{
+New-AzTenantDeployment -Name "assign-support-request-contributor-role" -Location "centralus" -TemplateFile "https://raw.githubusercontent.com/mvpsupport/CustomerGDAP/main/azuredeploy.json" -TemplateParameterObject @{
   principalId = "service_principal_object_id"
 }
